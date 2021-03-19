ba7cb1f9663378f5d3c72d48b58307f44a88cdcb
Mercer Denholm <mercer233@gmail.com>
2021-03-19 15:56:39 -0700

    Added form validation to register form

--- src/main/resources/static/javascript/components/forms/RegisterForm.tsx
+++ src/main/resources/static/javascript/components/forms/RegisterForm.tsx
@@ -1,138 +1,213 @@
-import React, { useState } from 'react';
-import { Notification, Progress } from 'rbx';
-const RegisterForm = () => {
-    const [error, setError] = useState<string>();
-    const [loading, setLoading] = useState<boolean>();
-    const [email, setEmail] = useState<string>('');
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const handleFormSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-        e.preventDefault();
+import React, { useEffect, useRef, useState } from 'react';
+import {
+    Notification,
+    Progress,
+    Help,
+    Card,
+    Label,
+    Input,
+    Field,
+    Button,
+} from 'rbx';
+import {
+    faCheck,
+    faFontAwesomeLogoFull,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useForm } from 'react-hook-form';
 
+type FormData = {
+    email: string;
+    username: string;
+    password: string;
+    passwordConfirm: string;
+};
+
+export default () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [apiError, setApiError] = useState<string>('');
+    const { register, setValue, handleSubmit, errors, watch } = useForm<
+        FormData
+    >({
+        criteriaMode: 'all',
+    });
+    const password = useRef({});
+    password.current = watch('password', '');
+
+    const onSubmit = handleSubmit((data) => {
         // register(email,username, password).catch((loginError) => {
         //   setError(loginError);
         // });
 
         //CSRF Protection
         const elementToken = document.querySelector('meta[name="_csrf"]');
-        const csrfToken = elementToken && elementToken.getAttribute("content");
-        const elementHeader = document.querySelector('meta[name="_csrf_header"]');
-        const csrfHeader = elementHeader && elementHeader.getAttribute("content");
-        console.log(csrfHeader);
-        console.log(csrfToken);
-        if(!csrfToken || !csrfHeader){
+        const csrfToken = elementToken && elementToken.getAttribute('content');
+        const elementHeader = document.querySelector(
+            'meta[name="_csrf_header"]'
+        );
+        const csrfHeader =
+            elementHeader && elementHeader.getAttribute('content');
 
-            setError("Missing CSRF token. Unable to send request.")
+        if (!csrfToken || !csrfHeader) {
+            setApiError('Missing CSRF token. Unable to send request.');
             return;
         }
         setLoading(true);
         const url = '/api/user/registration';
-
 
         fetch(url, {
             method: 'post',
             mode: 'cors',
             headers: {
                 'Content-Type': 'application/json',
-                'X-XSRF-TOKEN': csrfToken
+                'X-XSRF-TOKEN': csrfToken,
             },
             body: JSON.stringify({
-                email,
-                username,
-                password
+                email: data.email,
+                username: data.username,
+                password: data.password,
             }),
         })
             .then((response) => {
                 setLoading(false);
                 if (response.ok) {
-                    setError('');
+                    setApiError('');
                     window.location.href = '/registerSuccess';
                 } else {
                     response.json().then((data: any) => {
-                        setError(data.message);
+                        setApiError(data.message);
                     });
                 }
             })
             .catch((err) => {
                 setLoading(false);
-                setError('An network error has occurred.');
+                setApiError('An network error has occurred.');
                 console.log(err);
             });
-    };
+    });
 
     return (
-        <div>
-            <div className="card">
-                <header className="card-header">
-                    <p className="card-header-title">Register</p>
-                    <a
-                        href="#"
-                        className="card-header-icon"
-                        aria-label="more options"
-                    >
-                        <span className="icon">
-                            <i
-                                className="fas fa-angle-down"
-                                aria-hidden="true"
-                            />
-                        </span>
-                    </a>
-                </header>
-                <form onSubmit={handleFormSubmit}>
-                    <div className="card-content">
-                        {loading && <Progress />}
-                        {error && (
-                            <Notification color="warning">{error}</Notification>
+        <Card>
+            <Card.Header>
+                <Card.Header.Title>Register</Card.Header.Title>
+            </Card.Header>
+
+            <form onSubmit={onSubmit}>
+                <Card.Content>
+                    {loading && <Progress />}
+                    {apiError && (
+                        <Notification color="danger">{apiError}</Notification>
+                    )}
+                    <Field>
+                        <Label>Email*</Label>
+                        <input
+                            className="input"
+                            name="email"
+                            maxLength={50}
+                            ref={register({
+                                required: 'You must specify an email',
+                                maxLength: 50,
+                            })}
+                            type="email"
+                        />
+                        {errors.email && (
+                            <Help color="danger">{errors.email.message}</Help>
                         )}
-                        <div className="field">
-                            <label className="label">Email</label>
-                            <input
-                                className="input"
-                                id="email"
-                                name="email"
-                                value={email}
-                                onChange={(
-                                    e: React.ChangeEvent<HTMLInputElement>
-                                ) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="field">
-                            <label className="label">Username</label>
-                            <input
-                                className="input"
-                                id="username"
-                                name="username"
-                                value={username}
-                                onChange={(
-                                    e: React.ChangeEvent<HTMLInputElement>
-                                ) => setUsername(e.target.value)}
-                            />
-                        </div>
-                        <div className="field">
-                            <label className="label">Password</label>
-                            <input
-                                className="input"
-                                id="password"
-                                name="password"
-                                value={password}
-                                onChange={(
-                                    e: React.ChangeEvent<HTMLInputElement>
-                                ) => setPassword(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <footer className="card-footer">
-                        <button
-                            className="button is-primary card-footer-item"
-                            type="submit"
-                        >
-                            Register
-                        </button>
-                    </footer>
-                </form>
-            </div>
-        </div>
+                    </Field>
+
+                    <Field>
+                        <Label>Username*</Label>
+                        <input
+                            className="input"
+                            name="username"
+                            maxLength={25}
+                            ref={register({
+                                required: 'You must specify a username',
+                                maxLength: 25,
+                            })}
+                        />
+                        {errors.username && (
+                            <Help color="danger">
+                                {errors.username.message}
+                            </Help>
+                        )}
+                    </Field>
+
+                    <Field>
+                        <Label>Password*</Label>
+                        <input
+                            className="input"
+                            name="password"
+                            maxLength={25}
+                            ref={register({
+                                required: 'You must specify a password',
+                                minLength: {
+                                    value: 12,
+                                    message:
+                                        'Your password must have 12 characters.',
+                                },
+                                maxLength: 25,
+                                validate: {
+                                    hasNum: (value) =>
+                                        new RegExp('(?=.*[0-9])').test(value) ||
+                                        'Needs nums',
+                                    hasLowercase: (value) =>
+                                        new RegExp('(?=.*[a-z])').test(value) ||
+                                        'Needs lowercase',
+                                    hasUppercase: (value) =>
+                                        new RegExp('(?=.*[A-Z])').test(value) ||
+                                        'Needs uppercase',
+                                },
+                            })}
+                        />
+                        <Help>
+                            Passwords must be:
+                            <ul>
+                                <li>At least 12 characters long</li>
+                                <li>At least 1 alphabetical character</li>
+                                <li>At least 1 number</li>
+                                <li>At least 1 upper/lower case character</li>
+                            </ul>
+                        </Help>
+                        {errors.password && (
+                            <Help color="danger">
+                                {errors.password.message}
+                            </Help>
+                        )}
+                    </Field>
+
+                    <Field>
+                        <Label>Repeat Password*</Label>
+                        <input
+                            className="input"
+                            name="passwordConfirm"
+                            maxLength={25}
+                            ref={register({
+                                required: 'You must repeat your password',
+                                maxLength: 25,
+                                validate: (value) =>
+                                    value === password.current ||
+                                    'The passwords do not match',
+                            })}
+                            type="password"
+                        />
+                        {errors.passwordConfirm && (
+                            <Help color="danger">
+                                {errors.passwordConfirm.message}
+                            </Help>
+                        )}
+                    </Field>
+                </Card.Content>
+                <Card.Footer>
+                    <Button
+                        className="card-footer-item"
+                        color="primary"
+                        type="submit"
+                    >
+                        Register
+                    </Button>
+                </Card.Footer>
+            </form>
+        </Card>
     );
 };
-
-export default RegisterForm;
