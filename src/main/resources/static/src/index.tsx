4750f3a851c463f53b88b0bca15b17d37f506e0f
Mercer Denholm <mercer233@gmail.com>
2021-03-20 14:53:29 -0700

    changed primary color to purple, remove cover pic from profile, cleaned commented code out

--- src/main/resources/static/src/index.tsx
+++ src/main/resources/static/src/index.tsx
@@ -10,28 +10,9 @@ const RegisterForm = React.lazy(() =>
     import('../javascript/components/forms/RegisterForm')
 );
 
-// const CreatePostForm = React.lazy(() =>
-//     import('../javascript/components/forms/CreatePostForm')
-// );
-
 const NavbarAccountDropdown = React.lazy(() =>
     import('../javascript/components/NavbarAccountDropdown')
 );
 
-// const ReactNavbarAccountDropDownContainer = document.getElementById(
-//     'ReactNavbarAccountDropdown'
-// );
-// if (ReactNavbarAccountDropDownContainer) {
-//     ReactDOM.render(
-//         <Suspense fallback={<div>loading...</div>}>
-//             <NavbarAccountDropdown
-//                 username={ReactNavbarAccountDropDownContainer.getAttribute(
-//                     'data-username'
-//                 )}
-//             />
-//         </Suspense>,
-//         ReactNavbarAccountDropDownContainer
-//     );
-// }
 reactDomRender(NavbarAccountDropdown, 'ReactNavbarAccountDropdown');
 reactDomRender(RegisterForm, 'ReactRegisterForm');
