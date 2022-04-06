09c0330d31e28616d900c6f60f709524252cc7ab
TowelMan-public <OdagiriTomoya@outlook.jp>
2022-04-06 23:46:56 +0900

    Delete original java code

--- src/main/java/com/matamercer/microblog/web/error/exceptions/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.matamercer.microblog.web.error.exceptions;
-
-public class AuthenticationException extends org.springframework.security.core.AuthenticationException {
-    public AuthenticationException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-}
