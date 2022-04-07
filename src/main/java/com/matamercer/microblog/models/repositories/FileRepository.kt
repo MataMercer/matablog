e54bd88a8460c414387f85f31aea642c8dcc8a19
SoyNegro <krisoul.valdes@gmail.com>
2022-04-07 10:45:07 -0400

    cleaning code

--- src/main/java/com/matamercer/microblog/models/repositories/FileRepository.kt
+++ src/main/java/com/matamercer/microblog/models/repositories/FileRepository.kt
@@ -1,12 +1,7 @@
 package com.matamercer.microblog.models.repositories
 
-import com.matamercer.microblog.models.entities.Blog
 import com.matamercer.microblog.models.entities.File
-import com.matamercer.microblog.models.enums.PostCategory
-import com.matamercer.microblog.models.entities.PostTag
 import org.springframework.data.jpa.repository.JpaRepository
-import com.matamercer.microblog.models.entities.Follow
-import org.springframework.stereotype.Repository
 
-@Repository
+
 interface FileRepository : JpaRepository<File, Long>
