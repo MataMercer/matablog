f56821eb06ed0d8445227b481136c414c12cfcd4
SoyNegro <krisoul.valdes@gmail.com>
2022-04-07 11:07:56 -0400

    cleaning code

--- src/main/java/com/matamercer/microblog/web/api/v1/dto/responses/FileResponseDto.kt
+++ src/main/java/com/matamercer/microblog/web/api/v1/dto/responses/FileResponseDto.kt
@@ -1,14 +1,5 @@
 package com.matamercer.microblog.web.api.v1.dto.responses
 
-import lombok.EqualsAndHashCode
-import com.matamercer.microblog.web.api.v1.dto.responses.BaseModelResponseDto
-import com.matamercer.microblog.web.api.v1.dto.responses.BlogResponseDto
-import com.matamercer.microblog.web.api.v1.dto.responses.PostTagResponseDto
-import com.matamercer.microblog.web.api.v1.dto.responses.FileResponseDto
-import com.matamercer.microblog.web.api.v1.dto.responses.LikeResponseDto
-import com.matamercer.microblog.web.api.v1.dto.responses.PostResponseDto
-import lombok.Data
-
 class FileResponseDto : BaseModelResponseDto() {
     var name: String? = null
 }
