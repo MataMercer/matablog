7fc58100ce15dc9c3a9d5a86089d07aa886cd96f
SoyNegro <krisoul.valdes@gmail.com>
2022-04-13 14:58:28 -0400

    removed Optional returns on query

--- src/main/java/com/matamercer/microblog/models/repositories/BlogRepository.kt
+++ src/main/java/com/matamercer/microblog/models/repositories/BlogRepository.kt
@@ -2,8 +2,7 @@ package com.matamercer.microblog.models.repositories
 
 import com.matamercer.microblog.models.entities.Blog
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface BlogRepository : JpaRepository<Blog?, Long?> {
-    fun findByBlogName(blogName: String?): Optional<Blog?>?
+    fun findByBlogName(blogName: String?): Blog?
 }
