087030078796126e7566b972639be0c48d1292ad
SoyNegro <krisoul.valdes@gmail.com>
2022-04-13 14:38:45 -0400

    named field assignment

--- src/main/java/com/matamercer/microblog/configuration/SeederConfig.kt
+++ src/main/java/com/matamercer/microblog/configuration/SeederConfig.kt
@@ -4,41 +4,47 @@ import com.matamercer.microblog.models.entities.AuthenticationProvider
 import com.matamercer.microblog.models.entities.User
 import com.matamercer.microblog.models.repositories.UserRepository
 import com.matamercer.microblog.security.authorization.UserRole
-import com.matamercer.microblog.services.UserService
-import com.matamercer.microblog.services.PostService
 import com.matamercer.microblog.services.BlogService
+import com.matamercer.microblog.services.PostService
+import com.matamercer.microblog.services.UserService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
-class SeederConfig(private val userRepository: UserRepository, private val passwordEncoder: PasswordEncoder, private val userService: UserService, private val postService: PostService, private val blogService: BlogService) {
+class SeederConfig(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val userService: UserService,
+    private val postService: PostService,
+    private val blogService: BlogService
+) {
     @Bean
     fun seedData(): CommandLineRunner {
         return CommandLineRunner {
             val adminUser = User(
-                    "developer.mercer@googlemail.com",
-                    "a",
-                    passwordEncoder.encode("1"),
-                    UserRole.ADMIN,
-                    AuthenticationProvider.LOCAL
+                email = "developer.mercer@googlemail.com",
+                username = "a",
+                password = passwordEncoder.encode("1"),
+                role = UserRole.ADMIN,
+                authenticationProvider = AuthenticationProvider.LOCAL
             )
             addUserToApp(adminUser)
             val bloggerUser = User(
-                    "mercer233@gmail.com",
-                    "b",
-                    passwordEncoder.encode("1"),
-                    UserRole.BLOGGER,
-                    AuthenticationProvider.LOCAL
+                email = "mercer233@gmail.com",
+                username = "b",
+                password = passwordEncoder.encode("1"),
+                role = UserRole.BLOGGER,
+                authenticationProvider = AuthenticationProvider.LOCAL
             )
             addUserToApp(bloggerUser)
             val readerUser = User(
-                    "mercer233@googlemail.com",
-                    "c",
-                    passwordEncoder.encode("1"),
-                    UserRole.READER,
-                    AuthenticationProvider.LOCAL
+                email = "mercer233@googlemail.com",
+                username = "c",
+                password = passwordEncoder.encode("1"),
+                role = UserRole.READER,
+                authenticationProvider = AuthenticationProvider.LOCAL
             )
             addUserToApp(readerUser)
         }
